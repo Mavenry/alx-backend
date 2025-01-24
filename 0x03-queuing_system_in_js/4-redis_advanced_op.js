c8e933a7635e564ee71d2f0b4f5feeccb398079c
Mavenry <sundaybukola10@gmail.com>
2025-01-24 05:00:55 -0800

    solution added

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -1,44 +1,19 @@
-import { print, createClient } from 'redis';
-import { promisify } from 'util';
+import { createClient, print } from 'redis';
 
 const redisClient = createClient();
 
 redisClient.on('error', (error) => {
   console.log(`Redis client not connected to server: ${error.message}`);
-  redisClient.quit();
 });
-
-const getAsync = promisify(redisClient.get).bind(redisClient);
-
-/**
- * Set a key-value pair in redis
- * @param {string} schoolName - key
- * @param {string} value      - value
- */
-function setNewSchool(schoolName, value) {
-  redisClient.set(schoolName, value, print);
-}
-
-/**
- * Gets and display the value associated with given key
- * in redis store - asynchronously.
- * @param {string} schoolName - key to search in redis
- */
-async function displaySchoolValue(schoolName) {
-  const value = await getAsync(schoolName);
-  if (value) console.log(value);
-}
-
-/**
- * Entry point
- */
-async function main() {
-  await displaySchoolValue('Holberton');
-  setNewSchool('HolbertonSanFrancisco', '100');
-  await displaySchoolValue('HolbertonSanFrancisco');
-}
 
 redisClient.on('connect', () => {
   console.log('Redis client connected to the server');
-  main();
 });
+
+redisClient.hset('HolbertonSchools', 'Portland', 50, print);
+redisClient.hset('HolbertonSchools', 'Seattle', 80, print);
+redisClient.hset('HolbertonSchools', 'New York', 20, print);
+redisClient.hset('HolbertonSchools', 'Bogota', 20, print);
+redisClient.hset('HolbertonSchools', 'Cali', 40, print);
+redisClient.hset('HolbertonSchools', 'Paris', 42, print);
+redisClient.hgetall('HolbertonSchools', (_error, value) => console.log(value));
